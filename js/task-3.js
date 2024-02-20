5f9500593ccadba8455c3ddea622f89cedac4505
Igan22 <igankrasin@gmail.com>
2024-02-20 23:23:31 +0200

    Added changes to the JS

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,9 @@
 function getElementWidth(content, padding, border) {
-    return Number(content) + (Number(padding) + Number(border)) * 2;
+
+    return Number(content.replace("px", "")) + (Number(padding.replace("px", "")) + Number(border.replace("px", ""))) * 2;
 }
+
+
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
