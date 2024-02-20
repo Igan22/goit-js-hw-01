4fd4bae3256a4c4eae53670b9e54449f01054217
Igan22 <igankrasin@gmail.com>
2024-02-20 22:38:11 +0200

    Add

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,5 @@
 function getElementWidth(content, padding, border) {
-    return Number(content) + (Namber(padding) + Number(border)) * 2;
+    return Number(content) + (Number(padding) + Number(border)) * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
